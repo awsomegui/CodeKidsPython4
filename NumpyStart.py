3dfc780d67ce0382420f8e360b0bf6353e1140a6
Hugo <hugo@haaxman.co.uk>
2024-04-22 19:25:14 +0100

    commit

--- NumpyStart.py
+++ NumpyStart.py
@@ -6,3 +6,14 @@ Author: Hugo
 
 import numpy as np
 
+
+def ShowInformation():
+    print(f'Numpy Version: {np.__version__}')
+    print('\nConfig Settings:')
+    print(np.show_config())
+
+
+if __name__ == '__main__':
+    ShowInformation()
+
+    myFirstArray = np.array([1,2,3,4])
