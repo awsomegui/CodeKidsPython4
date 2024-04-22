7831fe6c78468506693471186013e8ffef26e561
Hugo <hugo@haaxman.co.uk>
2024-04-22 19:40:12 +0100

    commit

--- NumpyStart.py
+++ NumpyStart.py
@@ -12,8 +12,13 @@ def ShowInformation():
     print('\nConfig Settings:')
     print(np.show_config())
 
+def ContainsAZero(array: np.array):
+    return np.all(array)
+
 
 if __name__ == '__main__':
     ShowInformation()
 
     myFirstArray = np.array([1,2,3,4])
+    print(f'Array: {myFirstArray}')
+    print(f'Contains A Zero: {ContainsAZero(myFirstArray)}')
